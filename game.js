4570ee5976fc8997c1528e04b8d9854de64a6a9b
etsenx <51209299+etsenx@users.noreply.github.com>
2023-01-24 15:19:38 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,85 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+let level = 0;
+
+$(document).keydown(startGame);
+$("button").click(startGame);
+
+$(".btn").click(function (event) {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function startGame() {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+  $("button").hide();
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+
+  level++;
+  $("h1").html("Level " + level);
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    //Check If User Clicked all the pattern
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    //When Game Over
+    gameOver();
+  }
+}
+
+function gameOver() {
+  playSound("wrong");
+  $("body").addClass("game-over");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 100);
+  $("h1").html("Game Over, Press Restart");
+  level = 0;
+  started = false;
+  gamePattern = [];
+  $("button").text("Restart");
+  $("button").show();
+}
